862b2202f606be8ef638e916b14936cb6b5d446e
Chris Rawles <crawles@gmail.com>
2016-07-20 17:51:02 -0400

    Update prefs

--- jupyterPrefs.js
+++ jupyterPrefs.js
@@ -1,4 +1,4 @@
-logo = document.getElementById("kernel_logo_widget").getElementsByClassName("current_kernel_logo")[0]
-logo.src = "https://github.com/crawles/Logos/blob/master/Pivotal_Python_138x40.png?raw=true"
-logo.style = "display: inline; width:138px; height:40px"
-logo.innerHTML = '<a href="http://pivotal.io/data-science">' + logo.innerHTML + '</a>'
+logo = document.getElementById("kernel_logo_widget").getElementsByClassName("current_kernel_logo")[0];
+logo.src = "https://github.com/crawles/Logos/blob/master/Pivotal_Python_138x40.png?raw=true";
+logo.style = "display: inline; width:138px; height:40px";
+logo.innerHTML = '<a href="http://pivotal.io/data-science">' + logo.innerHTML + '</a>';
